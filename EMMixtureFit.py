5ef8a5561c70ae89079a167c7f569d3508f272a6
ZachGlassman <zach.glassman@gmail.com>
2015-07-28 15:22:10 -0400

    new mixture fitting
    added a 2d set

--- EMMixtureFit.py
+++ EMMixtureFit.py
@@ -14,6 +14,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.style.use('ggplot')
+import sys
 
 
 #first define normalized probability distributions
@@ -45,17 +46,17 @@ def e_step(data,qc, mu,sigsqrT,sigsqrN,q):
     return qc
 
       
-def m_step(data,qc, mu,sigsqrT,sigsqrN,q):
+def m_step(m,data,qc, mu,sigsqrT,sigsqrN,q):
     """now we compute the new parameters with the given posterior distributions"""
     mu_t = (data * qc['T']).sum()/qc['T'].sum()
-    sigsqrT_new = 3 *(data-mu_t)**2 * qc['T']
+    sigsqrT_new = m *(data-mu_t)**2 * qc['T']
     sigsqrN_new = (data-mu_t)**2 * qc['N']
     #now normalize
 
     sigsqrT_new = sigsqrT_new.sum()/qc['T'].sum()
     sigsqrN_new = sigsqrN_new.sum()/qc['N'].sum()
     
-    q = 1- qc['N'].sum()/len(qc['N'])
+    q = qc['T'].sum()/len(qc['T'])
     improvement = max(np.abs(sigsqrT_new-sigsqrT),np.abs(sigsqrN_new-sigsqrN))
     return improvement, {'mu':mu_t,'sigsqrT':sigsqrT_new,'sigsqrN':sigsqrN_new,'q':q}
 
@@ -75,48 +76,84 @@ def get_data(filename):
     pk = data/np.sum(data)
     dist = stats.rv_discrete(name = 'bimod' , values = (xk,pk))
     
-    return dist.rvs(size = 10000),pk,data_in,np.sum(data)
+    return dist.rvs(size = 100000), pk, data_in, np.sum(data)
+    
+def do_em(sim_data,qc,params,niter,m, eps = None):
+    if niter == False:
+        n_iter = 0
+        dif = 1
+        while dif > eps:
+            qc = e_step(sim_data,qc,**params)
+            dif, params = m_step(m,sim_data,qc,**params)
+            n_iter += 1
+    else:
+        for _ in range(niter):
+            qc = e_step(sim_data,qc,**params)
+            dif, params = m_step(m,sim_data,qc,**params)
+            
+    return params
+    
+#fancy writeout
+def write_progress(step,total,string = None):
+    """write the progress out to the window"""
+    perc_done = step/(total) * 100
+    #50 character string always
+    num_marks = int(.5 * perc_done)
+    out = ''.join('#' for i in range(num_marks))
+    out = out + ''.join(' ' for i in range(50 - num_marks))
+    sys.stdout.write('\r[{0}]{1:>2.0f}% {2}'.format(out,perc_done,string))
+    sys.stdout.flush()
     
 def main():
-    sim_data,data,raw_data,sum_data = get_data('C:\\Users\zag\\Documents\\BECFitting\\2015-7-14\\7-14-matrix0108_0.txt')
+    sim_data,data,raw_data,sum_data = get_data('C:\\Users\zag\\Documents\\BECFitting\\2015-7-14\\7-14-matrix0104_0.txt')
     #need to keep track of qc for both TF and Normal components
     qc = {'T':np.zeros(len(sim_data)),'N':np.zeros(len(sim_data))}
-    params = {'mu':np.mean(sim_data),'sigsqrT':19**2,'sigsqrN':2500**2,'q':.5}
-    dif = 1
-    eps = 1e-20
-    n_iter = 0
-    #while dif > eps:
-    for _ in range(100):
-        qc = e_step(sim_data,qc,**params)
-        #print((1-params['q'])*np.log(norm_dist(data,params['mu'],params['sigsqrN'])).sum()+params['q']*np.log(tf_dist(data,params['mu'],params['sigsqrT'])).sum())
-        dif, params = m_step(sim_data,qc,**params)
-        n_iter += 1
-    
+    params = {'mu':np.mean(sim_data),'sigsqrT':9**2,'sigsqrN':25**2,'q':.5}
+    ans = {}
+    m = np.linspace(4,6,1000)
+    index = 0
+    tot_index = len(m)
+    write_progress(index,tot_index,m[0])
+    for j in m:
+        ans[j] = do_em(sim_data,qc,params.copy(),50,j,1e-20)
+        index += 1
+        write_progress(index,tot_index,m[index-1])
         
-    print('Number of iterations: {0}'.format(n_iter))
-    print(format_params(params))
-    print('Thomas Fermi radius is : {0}'.format(np.sqrt(params['sigsqrT'])))
-    print('Gaussian 1/e^2 radius is : {0}'.format(np.sqrt(params['sigsqrN'])))
-    mu = params['mu']+60
-    sigsqrN = params['sigsqrN']
-    sigsqrT = params['sigsqrT']
-    q = params['q']
-
+    fig, ax = plt.subplots(3,1)
     x = np.arange(len(raw_data))
-    norm = (1-q)*norm_dist(x,mu,sigsqrN)
-    tf =   q*tf_dist(x,mu,sigsqrT)
-    
-    fig, ax = plt.subplots(2,1)
-    #plt.hist(sim_data,normed = True, bins = len(data))
-    #ax[0].plot(x,data,'o-',label = 'data')
-    ax[0].plot(x,tf,label = 'TF')
-    ax[0].plot(x,norm,label = 'Norm')
-    ax[0].plot(x, norm+tf,label = 'Fitted')
+    ax[0].hist(sim_data+60,normed = True, bins = len(data))
     ax[1].plot(x,raw_data,'o-')
+    minj = [0,100000]
+    for j in ans.keys():
+        mu = ans[j]['mu']+60
+        sigsqrN = ans[j]['sigsqrN']
+        sigsqrT = ans[j]['sigsqrT']
+        q = ans[j]['q']
+        
+        norm = (1-q)*norm_dist(x,mu,sigsqrN)
+        tf =   q*tf_dist(x,mu,sigsqrT)
+        ans[j]['chisqr'] = (((norm+tf)*sum_data - raw_data)**2).sum()
+        if ans[j]['chisqr']  < minj[1]:
+            minj[0] = j
+            minj[1] = ans[j]['chisqr']
+     
+    j = minj[0]
+    print('\nOptimal scaling is : {0}'.format(j))
+    mu = ans[j]['mu']+60
+    sigsqrN = ans[j]['sigsqrN']
+    sigsqrT = ans[j]['sigsqrT']
+    q = ans[j]['q'] 
+    norm = (1-q)*norm_dist(x,mu,sigsqrN)
+    tf =   q*tf_dist(x,mu,sigsqrT)       
+    ax[0].plot(x,tf,label = 'TF {0}'.format(j))
+    ax[0].plot(x,norm,label = 'Norm {0}'.format(j))
+    ax[0].plot(x, norm+tf,label = 'Fitted {0}'.format(j))
     ax[1].plot(x,(norm+tf)*sum_data)
-    ax[0].legend()
+        
+    ax[2].plot(m,[ans[i]['chisqr'] for i in m])    
+    #ax[0].legend()
     plt.show()
-    
+
    
 if __name__ == '__main__':
     main()
